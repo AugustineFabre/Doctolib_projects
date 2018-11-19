2e8edd9b7864bbc3c3435316329f817fce098a6a
ulrichYepmou <45000634+ulrichYepmou@users.noreply.github.com>
2018-11-19 15:06:07 +0100

    Update and rename test_nombre_de_fonction.py to 2.test_nombre_de_fonction.py

--- 2.test_nombre_de_fonction.py
+++ 2.test_nombre_de_fonction.py
@@ -1,5 +1,5 @@
 def test_nombre_de_fonctions():
-    code_a_tester=openRb('/Users/yepmoujj/PycharmProjects/DoctoLib Recruitment/event_candidate_a') ## prends en entrée le code contenant 10 fonctions
+    code_a_tester=openRb('/Users/yepmoujj/PycharmProjects/DoctoLib Recruitment/event_candidate_a') ## prend en entrée le code contenant 10 fonctions
     if nombre_fonction(code_a_tester)==10:
         print("fonction validée")
     else:
