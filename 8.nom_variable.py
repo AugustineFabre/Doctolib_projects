409b52345e7d193e06d221be06fef994a3f2d6af
AugustineFabre <45000556+AugustineFabre@users.noreply.github.com>
2018-11-19 15:47:22 +0000

    Update 8.nom_variable.py

--- 8.nom_variable.py
+++ 8.nom_variable.py
@@ -1,7 +1,7 @@
 def nom_variable(code):
     liste_variable=[]
     liste_ligne=code.split("\n")
-    for ligne in liste_ligne:
+    for ligne in liste_ligne: #on récupère les variables des "def" 
         if "def " in ligne:
             if "(" in ligne:
                 if "()" not in ligne:
@@ -12,14 +12,14 @@ def nom_variable(code):
                             n2=i
                     liste_intermediaire=ligne[n1+1:n2].split(',')
                     for element in liste_intermediaire:
-                        if "=" in element:
+                        if "=" in element: # pour les variables par défaut on ne prend que le nom de la variable
                             for i in range (0,len(element)):
                                 if element[i]=="=":
                                     n=i
                             liste_variable.append(element[0:n-1])
                         else:
                             liste_variable.append(element)
-        if "def" not in ligne:
+        if "def" not in ligne: #on récupère les variables définies par les =
             for i in range (0, len(ligne)-2):
                 n1=ligne[i]
                 n2=ligne[i+1]
@@ -29,7 +29,7 @@ def nom_variable(code):
                     if "'" not in ligne[0:i]:
                         variable=ligne[0:i]
                         liste_variable.append(variable)
-        if '|' in ligne:
+        if '|' in ligne: #on récupère les variables définies par les |
             n1=0
             n2=0
             for i in range (0,len(ligne)):
@@ -40,7 +40,7 @@ def nom_variable(code):
                     if ligne[i]=="|":
                         n2=i
             liste_variable.append(ligne[n1+1:n2])
-    liste_variable=[a.replace(" ","") for a in liste_variable]
+    liste_variable=[a.replace(" ","") for a in liste_variable] # on supprime les espaces en trop
     liste_variable=[a.replace("self.","") for a in liste_variable]
     return(liste_variable)
 
