00dd8693331af7997d89785129557837bb3d3c4c
AugustineFabre <45000556+AugustineFabre@users.noreply.github.com>
2018-11-19 10:58:09 +0000

    Add files via upload

---
+++ nombre_test.py
@@ -0,0 +1,12 @@
+def nombre_fonction(tests):
+    compteur=0
+    for i in range (0,len(tests)-3):
+        elem1=tests[i]
+        elem2=tests[i+1]
+        elem3=tests[i+2]
+        elem4=tests[i+3]
+        liste=elem1+elem2+elem3+elem4
+        if liste=="test":
+            compteur=compteur+1
+    return (compteur)
+
