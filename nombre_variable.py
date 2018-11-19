324313c575bf7ccca59a95f48ef09a38a78deedd
ConstanceReb <45164564+ConstanceReb@users.noreply.github.com>
2018-11-19 15:30:30 +0100

    Add files via upload

---
+++ nombre_variable.py
@@ -0,0 +1,30 @@
+def nombre_variable(code):
+    compteur=0
+    liste_ligne=code.split("\n")
+    for ligne in liste_ligne:
+        if "def " in ligne:
+            if "(" in ligne:
+                if "()" not in ligne:
+                    for caractere in ligne:
+                        if caractere == ",":
+                            compteur+=1
+                    compteur+=1
+        if "def" not in ligne:
+            for i in range (0, len(ligne)-2):
+                n1=ligne[i]
+                n2=ligne[i+1]
+                n3=ligne[i+2]
+                mot=n1+n2+n3
+                if mot==' = ':
+                    compteur+=1
+        if '|' in ligne:
+            compteur+=1
+    return compteur
+
+
+
+
+
+
+
+
