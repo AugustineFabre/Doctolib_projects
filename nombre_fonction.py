a8e2e445a927bdf171514dbb5edd7eaade6750bd
ConstanceReb <45164564+ConstanceReb@users.noreply.github.com>
2018-11-19 14:04:22 +0100

    Update nombre_fonction.py

--- nombre_fonction.py
+++ nombre_fonction.py
@@ -1,16 +1,17 @@
 def nombre_fonction(code):
     compteur=0
-    for i in range (0,len(code)-2):
-        elem1=code[i]
-        elem2=code[i+1]
-        elem3=code[i+2]
-        liste=elem1+elem2+elem3
-        if liste=="def":
-            compteur=compteur+1
+    if len(code)>=3:
+        for i in range (0,len(code)-3):
+            elem1=code[i]
+            elem2=code[i+1]
+            elem3=code[i+2]
+            elem4=code[i+3]
+            liste=elem1+elem2+elem3+elem4
+            if liste=="def ":
+                compteur=compteur+1
     return (compteur)
 
 
-code="frdef njii codef"
 
 
 
