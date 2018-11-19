f3c41b52e64e19d623e06d86c3cdd64c684e333e
ChaoWANG0511 <45005628+ChaoWANG0511@users.noreply.github.com>
2018-11-19 14:15:05 +0100

    Add files via upload

---
+++ test_open_ruby.py
@@ -0,0 +1,9 @@
+from open_ruby import *
+
+print("this is the candidate's code:\n")
+print(candidate_rb)
+print("\n")
+print("this is the candidate's test:\n")
+print(candidate_rb_test)
+
+print(type(candidate_rb))
