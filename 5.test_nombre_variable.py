7f76237ae1de928c761a01d9ae116a4addbda4f0
AugustineFabre <45000556+AugustineFabre@users.noreply.github.com>
2018-11-19 14:44:20 +0000

    Update 5.test_nombre_variable.py

--- 5.test_nombre_variable.py
+++ 5.test_nombre_variable.py
@@ -1,5 +1,5 @@
 
-def test("bdhzhe \n def de() \n def fze(d,vr) \n def cnezibr(ncuze)\n cnjzie|czyuer \n cre = cez"):
+def test("bdhzhe \n def de() \n def fze(d,vr) \n def cnezibr(ncuze)\n cnjzie|czyuer \n cre = cez \n def dre(cz,vrdf = erv) "):
 
   if nombre_variable("bdhzhe \n def de() \n def fze(d,vr) \n def cnezibr(ncuze)\n cnjzie|czyuer")==5:
 
