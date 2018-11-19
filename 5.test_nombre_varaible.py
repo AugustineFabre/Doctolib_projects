0c58a8f40a7f04b67b515ac9b9ded7d56b0521b7
AugustineFabre <45000556+AugustineFabre@users.noreply.github.com>
2018-11-19 14:15:59 +0000

    Delete 5.test_nombre_varaible.py

--- 5.test_nombre_varaible.py
+++
@@ -1,4 +0,0 @@
-def test("bdhzhe \n def de() \n def fze(d,vr) \n def cnezibr(ncuze)\n cnjzie|czyuer \n cre = cez"):
-  if nombre_variable("bdhzhe \n def de() \n def fze(d,vr) \n def cnezibr(ncuze)\n cnjzie|czyuer")==5:
-    print "fonction bien executée"
-    
