dd4879761001a7c4a228856420f1b0090350d541
ulrichYepmou <45000634+ulrichYepmou@users.noreply.github.com>
2018-11-19 15:05:29 +0100

    Add files via upload

---
+++ test_nombre_de_fonction.py
@@ -0,0 +1,8 @@
+def test_nombre_de_fonctions():
+    code_a_tester=openRb('/Users/yepmoujj/PycharmProjects/DoctoLib Recruitment/event_candidate_a') ## prends en entrée le code contenant 10 fonctions
+    if nombre_fonction(code_a_tester)==10:
+        print("fonction validée")
+    else:
+        print("erreur")
+
+print(test_nombre_de_fonctions())
