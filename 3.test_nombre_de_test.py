6b8fa2155db8d768372ea306107746074c137d8f
ulrichYepmou <45000634+ulrichYepmou@users.noreply.github.com>
2018-11-19 15:16:52 +0100

    Update 3.test_nombre_de_test.py

--- 3.test_nombre_de_test.py
+++ 3.test_nombre_de_test.py
@@ -1,6 +1,6 @@
 def test_nombre_de_tests():
     code_a_tester=openRb('/Users/yepmoujj/PycharmProjects/DoctoLib Recruitment/event_candidate_a') ## prends en entrée le code contenant 16 tests
-    if nombre_fonction(code_a_tester)==16:
+    if nombre_test(code_a_tester)==16:
         print("fonction validée")
     else:
         print("erreur")
