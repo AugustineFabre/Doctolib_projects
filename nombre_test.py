d31569d29d8b19949e679d50ddccb394247988fc
AugustineFabre <45000556+AugustineFabre@users.noreply.github.com>
2018-11-19 13:07:05 +0000

    Update nombre_test.py

--- nombre_test.py
+++ nombre_test.py
@@ -1,12 +1,13 @@
 def nombre_fonction(tests):
     compteur=0
-    for i in range (0,len(tests)-3):
-        elem1=tests[i]
-        elem2=tests[i+1]
-        elem3=tests[i+2]
-        elem4=tests[i+3]
-        liste=elem1+elem2+elem3+elem4
-        if liste=="test":
-            compteur=compteur+1
+    if len(tests)>=4:
+        for i in range (0,len(tests)-3):
+            elem1=tests[i]
+            elem2=tests[i+1]
+            elem3=tests[i+2]
+            elem4=tests[i+3]
+            liste=elem1+elem2+elem3+elem4
+            if liste=="test":
+                compteur=compteur+1
     return (compteur)
 
