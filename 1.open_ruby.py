4f2e334361514d7963354458a7436bfcde7f5f16
ulrichYepmou <45000634+ulrichYepmou@users.noreply.github.com>
2018-11-19 16:41:41 +0100

    Update 1.open_ruby.py

--- 1.open_ruby.py
+++ 1.open_ruby.py
@@ -1,9 +1,9 @@
-def openRb(filename):
+def openRb(filename): ## prend en entrée le chemin permettant d'accéder au fichier
     with open(filename) as f:
         read_data = f.read()
        # print(read_data)
         f.closed
-    return read_data
+    return read_data ## renvoie le code de type string
 
 candidate_rb = openRb('../fichier_ruby/event_candidate_a.rb.rb')
 candidate_rb_test = openRb('../fichier_ruby/event_candidate_a_test.rb.rb')
