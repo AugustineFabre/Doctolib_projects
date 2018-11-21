a76c898861a859c8e492d9f2aa8c502071f0c484
Guilhem Coutansais <guilhem.poiriercoutansais@supelec.fr>
2018-11-21 10:07:27 +0100

    added files

--- MVP1/put_in_json.py
+++ MVP1/put_in_json.py
@@ -1,15 +1,15 @@
 import json
 
-from num_functions import *
+from nombre_fonction import nombre_fonction
 functionCount = nombre_fonction(candidate_rb)
 
-from num_comments import *
+from nombre_commentaire import *
 commentCount = nombre_commentaire(candidate_rb)
 
-from num_tests import *
+from nombre_test import *
 testCount = nombre_test(candidate_rb_test)
 
-from num_variables import *
+from nombre_variable import *
 variableCount = nombre_variable(candidate_rb)
 
 MVP1 = {"functionCount": functionCount, "commentCount": commentCount, "variableCount": variableCount, "testCount": testCount}
