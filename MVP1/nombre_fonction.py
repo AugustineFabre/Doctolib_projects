3e1ad5d789b839bcbd0e5e7da7e4a9592be1d42f
ConstanceReb <45164564+ConstanceReb@users.noreply.github.com>
2018-11-23 11:07:01 +0100

    Update nombre_fonction.py

--- MVP1/nombre_fonction.py
+++ MVP1/nombre_fonction.py
@@ -1,13 +1,13 @@
 def nombre_fonction(code):
     compteur=0
-    if len(code)>=4:
+    if len(code)>=4:    #pour eviter qu'il y ait une erreur
         for i in range (0,len(code)-3):
             elem1=code[i]
             elem2=code[i+1]
             elem3=code[i+2]
             elem4=code[i+3]
-            liste=elem1+elem2+elem3+elem4
-            if liste=="def ":
+            liste=elem1+elem2+elem3+elem4    
+            if liste=="def ":       #on compare 4 caracteres d'affilee 
                 compteur=compteur+1
     return (compteur)
 
