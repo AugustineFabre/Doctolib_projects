dd3300983ed6fb4db35cabcf5047051061a655e9
AugustineFabre <45000556+AugustineFabre@users.noreply.github.com>
2018-11-23 10:10:27 +0000

    Delete points_noms.py

--- points_noms.py
+++
@@ -1,5 +0,0 @@
-def points_noms(code):
-
-    alpha1, num_meaningful_a1 = seulement_alpha(nom_variable(code))
-    alpha2, num_meaningful_a2 = seulement_alpha(nom_fonction(code))
-    return( (analyse_nom(alpha1,num_meaningful_a1))[1]/len(alpha1)*100 , (analyse_nom(alpha2,num_meaningful_a2))[1]/len(alpha2)*100 )
