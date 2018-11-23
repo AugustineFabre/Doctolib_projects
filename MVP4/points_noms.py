86d9642b6d0fdcba6d23d486b7ed67bddd721014
ConstanceReb <45164564+ConstanceReb@users.noreply.github.com>
2018-11-23 11:12:05 +0100

    Update points_noms.py

--- MVP4/points_noms.py
+++ MVP4/points_noms.py
@@ -1,4 +1,4 @@
-def points_noms(code):
+def points_noms(code):   #on attribue une note sur 100 pour la qualite des noms des variables/fonctions
 
     alpha1, num_meaningful_a1 = seulement_alpha(nom_variable(code))
     alpha2, num_meaningful_a2 = seulement_alpha(nom_fonction(code))
